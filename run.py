b2c8ada567f3718195123b649c442b1d73181dc7
Your Name <you@example.com>
2019-11-02 14:24:46 -0400

    Fixed size Issue

--- run.py
+++ run.py
@@ -10,27 +10,39 @@ account_sid = 'AC4cebdb3f91c465dd8de9ebebe399148c'
 auth_token = '6a42c296e68579a60d925ac8c39e61d3'
 client = Client(account_sid, auth_token)
 
+printMenu = 0
 app = Flask(__name__)
 
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    global printMenu
     number = request.form['From']
     message_body = request.form['Body']
 
-    payload = {'api_key': 'seXC2fGDNpVjAnTtg4qLhZiBvKqLTLzp', 'q': message_body, 'limit': 1, 'lang': 'en'}
-    r = requests.get('http://api.giphy.com/v1/gifs/search', params=payload)
-    print(r.url)
+    if printMenu == 0:
+        resp = MessagingResponse()
+        resp.message = (client.messages.create(
+            body="\nWelcome to MemeHotline!\n"
+                 "enter some keywords to find some memes",
+            from_='+14243214684',
+            to='+14848853093'))
+        printMenu = 1
+    else:
+        payload = {'api_key': 'seXC2fGDNpVjAnTtg4qLhZiBvKqLTLzp', 'q': message_body, 'limit': 1}
+        r = requests.get('http://api.giphy.com/v1/gifs/search', params=payload)
+        print(r.url)
+        #pprint.pprint(r.json()['data'][0]['images'])
 
-    def gotData(stuff):
-        print(stuff.json())
+        def gotData(stuff):
+            print(stuff.json())
 
-    resp = MessagingResponse()
-    resp.message = (client.messages.create(
-        body="Here you go!",
-        from_='+14243214684',
-        media_url=(r.json()['data'][0]['images']['original']['url']),
-        to='+14848853093'))
+        resp = MessagingResponse()
+        resp.message = (client.messages.create(
+            body="Here you go!",
+            from_='+14243214684',
+            media_url=(r.json()['data'][0]['images']['downsized']['url']),
+            to='+14848853093'))
     return str(resp)
 
 
