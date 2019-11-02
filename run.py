0d6c21f09e5604be18df21a35b5c49e86b34ddf7
Your Name <you@example.com>
2019-11-02 12:20:28 -0400

    working textGif

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+import requests
+import pprint
 from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC4cebdb3f91c465dd8de9ebebe399148c'
+auth_token = '6a42c296e68579a60d925ac8c39e61d3'
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
@@ -9,8 +18,19 @@ def sms():
     number = request.form['From']
     message_body = request.form['Body']
 
+    payload = {'api_key': 'seXC2fGDNpVjAnTtg4qLhZiBvKqLTLzp', 'q': message_body, 'limit': 1, 'lang': 'en'}
+    r = requests.get('http://api.giphy.com/v1/gifs/search', params=payload)
+    print(r.url)
+
+    def gotData(stuff):
+        print(stuff.json())
+
     resp = MessagingResponse()
-    resp.message('Fuck you, Hamza')
+    resp.message = (client.messages.create(
+        body="Here you go!",
+        from_='+14243214684',
+        media_url=(r.json()['data'][0]['images']['original']['url']),
+        to='+14848853093'))
     return str(resp)
 
 
