c90a6220622d65c487083a9498c01e45cbf47d3a
Hamza S <hmshaikh19@gmail.com>
2019-11-02 21:17:30 -0400

    updated the image edit files

--- Boost.py
+++ Boost.py
@@ -1,23 +1,24 @@
-from PIL import ImageFilter
+from PIL import ImageFilter, Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-img = Image.open(r"C:\Users\ryanb\OneDrive\IdeaProjects\Image Editing\Hack.jpg")
-im1 = img.filter(ImageFilter.BLUR)
-for x in range(0, 10):
-    im1 = im1.filter(ImageFilter.BLUR)
+img = Image.open('input.jpg')
+#im1 = img.filter(ImageFilter.BLUR)
+#for x in range(0, 10):
+#    im1 = im1.filter(ImageFilter.BLUR)
     
-width, height = im1.size
-for x in range(width):
-    for y in range(height):
-        r,g,b = im1.getpixel((x,y))
-        if r > 150:
-            value = (255, g, b)
-            im1.putpixel((x,y), value)
-        else:
-            value = (r + 105, g, b)
-            im1.putpixel((x,y), value)
+# width, height = im1.size
+# for x in range(width):
+#     for y in range(height):
+#         r,g,b = im1.getpixel((x,y))
+#         if r > 250:
+#             value = (255, g, b)
+#             im1.putpixel((x,y), value)
+#         else:
+#             value = (r + 3, g, b)
+#             im1.putpixel((x,y), value)
 
 
 
-imgplot = plt.imshow(im1)
+plt.imsave('output.jpg', img)
+img = 0
